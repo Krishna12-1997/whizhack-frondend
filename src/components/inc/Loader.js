3b310e59c861945545f2e6231164d42b26a0a937
Krishna <krishnagnagpure@gmail.com>
2024-01-10 18:18:46 +0530

    add css on About page

--- src/components/inc/Loader.js
+++ src/components/inc/Loader.js
@@ -7,7 +7,7 @@ import './loader.css';
 const Loader = ({ loading, children }) => {
   if (loading) {
     return (
-      <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh"}}>
+      <div className="d-flex justify-content-center align-items-center loader" style={{ height: "100vh"}}>
         <Lottie
           loop
           animationData={loader}
