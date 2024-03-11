6f8ddf1b44be816cedf1db0018c6b9888c98235f
Krishna <krishnagnagpure@gmail.com>
2024-03-11 16:21:17 +0530

    Add changes in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -28,7 +28,7 @@ import animationData from "../json/animation.json";
 
 export default function Home() {
   const uri =
-    "http://localhost:1337/api/home?populate=productleft.product_button, contact.product_type, link, images";
+    "https://test.whizhack.com/api/home?populate=productleft.product_button, contact.product_type, link, images";
   const { loading, error, data } = useFetch(uri);
 
   if (loading) {
